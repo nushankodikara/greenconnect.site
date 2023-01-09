18e541856edd15ed2d2aff7c3151c4a6118eece2
nushankodikara <nushankodi@gmail.com>
2023-01-09 09:31:03 +0530

    switched to production

--- src/servers.php
+++ src/servers.php
@@ -1,14 +1,15 @@
 <?php
 
 // Production
-// $server = "localhost";
-// $username = "greencon_user";
-// $password = "earcLVn22NeL";
-// $dbname = "greencon_greenconnect";
+$server = "localhost";
+$username = "greencon_user";
+$password = "earcLVn22NeL";
+$dbname = "greencon_greenconnect";
 
 // Development
-$server = "mysql";
-$username = "root";
-$password = "root";
-$dbname = "greenconnect";
+// $server = "mysql";
+// $username = "root";
+// $password = "root";
+// $dbname = "greenconnect";
+
 ?>
